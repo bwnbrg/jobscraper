a6198fbba53cf04e0f86d8b0aae0f3d69675fc7a
Ben <42478161+bwnbrg@users.noreply.github.com>
2025-06-24 14:20:00 -0400

    Update items.py
    Abstracted items.py class and added fields to enable richer metadata

--- job_scraper/job_scraper/items.py
+++ job_scraper/job_scraper/items.py
@@ -6,12 +6,17 @@
 import scrapy
 
 
-class LeverScraperItem(scrapy.Item):
+class JobItem(scrapy.Item):
     # define the fields for your item here like:
 
     title = scrapy.Field()
+    employment_type = scrapy.Field()
+    workplace_type = scrapy.Field()
     location = scrapy.Field()
     department = scrapy.Field()
     url = scrapy.Field()
     description = scrapy.Field()
     requirements = scrapy.Field()
+    company = scrapy.Field()
+    source = scrapy.Field()  # e.g. 'lever' or 'greenhouse'
+    scraped_at = scrapy.Field()
